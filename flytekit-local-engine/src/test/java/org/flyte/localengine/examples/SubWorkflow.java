a29305b8f6cf407b8251c93738ab85274813bdb7
Sonja Ericsson <sonjae@spotify.com>
2022-07-01 16:20:01 +0200

    example

---
+++ flytekit-local-engine/src/test/java/org/flyte/localengine/examples/SubWorkflow.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 Flyte Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.flyte.localengine.examples;
+
+import com.google.auto.service.AutoService;
+import org.flyte.flytekit.SdkBindingData;
+import org.flyte.flytekit.SdkWorkflow;
+import org.flyte.flytekit.SdkWorkflowBuilder;
+
+@AutoService(SdkWorkflow.class)
+
+public class SubWorkflow extends SdkWorkflow {
+
+  @Override
+  public void expand(SdkWorkflowBuilder builder) {
+    SdkBindingData a = builder.inputOfInteger("a");
+    SdkBindingData b = builder.inputOfInteger("b");
+    SdkBindingData c = builder.inputOfInteger("c");
+    SdkBindingData ab = builder.apply("sum-a-b", new SumTask().withInput("a", a).withInput("b", b)).getOutput("c");
+    SdkBindingData res  = builder.apply("sum-ab-c", new InnerSubWorkflow().withInput("a", ab).withInput("b", c)).getOutput("result");
+    builder.output("result", res);
+  }
+}
